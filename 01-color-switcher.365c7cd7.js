117847f09cdaee9a9ce9afaf95720a2da35a7c22
darynna <116767355+darynna@users.noreply.github.com>
2023-08-03 21:34:03 +0000

    Deploying to gh-pages from @ darynna/goit-js-hw-09@7714b0bec261a6d540533d088a6a5ca48be3df05 🚀

---
+++ 01-color-switcher.365c7cd7.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let a=null;t.addEventListener("click",(()=>{t.disabled=!0,a=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.365c7cd7.js.map
